4332006ae007dbd785c9e9070fa22912ec0a6f4d
Manoj Kumar <64196382+manojkumar-ally@users.noreply.github.com>
2021-10-22 09:21:33 +0530

    initial commit

--- src/viewsTable.js
+++ src/viewsTable.js
@@ -3,8 +3,7 @@ import useResize from "./useResize";
 
 import Table from "./Table";
 
-export default ({ data }) => {
-  const [objectives, updateObjectives] = useState(data);
+export default ({ data, groupKey }) => {
   const [expandStatus, setExpandStatus] = useState({});
 
   const [onElementClick] = useResize();
@@ -14,22 +13,20 @@ export default ({ data }) => {
   };
 
   const onRowDragEnd = (result) => {
-    const { source, destination } = result;
-
-    if (!destination) {
-      return;
-    }
-    if (
-      source.droppableId === destination.droppableId &&
-      source.index === destination.index
-    ) {
-      // user drags and drops at the same position
-      return;
-    }
-
-    const splicedObj = objectives.splice(source.index, 1);
-    objectives.splice(destination.index, 0, splicedObj[0]);
-    updateObjectives(objectives);
+    // const { source, destination } = result;
+    // if (!destination) {
+    //   return;
+    // }
+    // if (
+    //   source.droppableId === destination.droppableId &&
+    //   source.index === destination.index
+    // ) {
+    //   // user drags and drops at the same position
+    //   return;
+    // }
+    // const splicedObj = objectives.splice(source.index, 1);
+    // objectives.splice(destination.index, 0, splicedObj[0]);
+    // updateObjectives(objectives);
   };
 
   return (
@@ -38,7 +35,8 @@ export default ({ data }) => {
         <Table
           onElementClick={onElementClick}
           onRowDragEnd={onRowDragEnd}
-          objectives={objectives}
+          data={data}
+          groupKey={groupKey}
           expandStatus={expandStatus}
           toggle={toggle}
         />
